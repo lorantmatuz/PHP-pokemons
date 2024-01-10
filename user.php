b48fcf79e0424e9a8d61b8fa0626635da3435def
matuzl20 <matuzlorant20@gmail.com>
2024-01-10 21:33:34 +0100

    User page

--- user.php
+++ user.php
@@ -11,6 +11,7 @@
     $user = $storage->findById($_SESSION['user_id']);
   }
 
+  $cards = new Storage(new JsonIO("cards.json"), true);
 ?>
 
 <!DOCTYPE html>
@@ -26,27 +27,52 @@
 
 <body>
   <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Főoldal</a></li>
-                <?php if(isset($_SESSION['user_id'])): ?>
-                    <li><a href="user.php"><?= $_SESSION['user_id'] ?> -
-                        💰<?= $user['money'] ?></a></li>
-                  <li><a href="logout.php">Kijelentkezés</a></li>
-                <?php else: ?>
-                  <li><a href="login.php">Bejelentkezés</a></li>
-                  <li><a href="registration.php" >Regisztráció</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Főoldal</a></li>
+        <?php if(isset($_SESSION['user_id'])): ?>
+          <li><a href="user.php"><?= $_SESSION['user_id'] ?> -
+              💰<?= $user['money'] ?></a></li>
+          <li><a href="logout.php">Kijelentkezés</a></li>
+        <?php else: ?>
+          <li><a href="login.php">Bejelentkezés</a></li>
+          <li><a href="registration.php" >Regisztráció</a></li>
+        <?php endif; ?>
+      </ul>
+    </nav>
+  </header>
 
   <div id="content">
-
+    <h2>Felhasználó adatai</h2>
+    <ul>
+      <li>Felhasználónév: <?= $user['user_name'] ?></li>
+      <li>Email: <?= $user['email'] ?></li>
+      <li>Pénz: <?= $user['money'] ?></li>
+    </ul>
+    <h2> Kártyái </h2>
+    <div id="card-list">
+      <?php foreach($user['cards'] as $cardId):
+              $card = $cards->findById($cardId); ?>
+        <div class="pokemon-card">
+          <div class="image clr-<?= $card["type"] ?>">
+            <img src="<?= $card["image"] ?>" alt="">
+          </div>
+          <div class="details">
+              <h2><a href="details.php?id=<?= $cardId ?>"><?= $card["name"] ?></a></h2>
+              <span class="card-type"><span class="icon">🏷</span> <?= $card['type'] ?></span>
+              <span class="attributes">
+                  <span class="card-hp"><span class="icon">❤</span> <?= $card['hp'] ?></span>
+                  <span class="card-attack"><span class="icon">⚔</span> <?= $card['attack'] ?></span>
+                  <span class="card-defense"><span class="icon">🛡</span> <?= $card['defense'] ?></span>
+              </span>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
 
   <footer>
-      <p>IKémon | ELTE IK Webprogramozás</p>
+    <p>IKémon | ELTE IK Webprogramozás</p>
   </footer>
 </body>
 
